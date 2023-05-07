8d170f2736d37f1905cfeb4669f53ec4d6fd9c09
Enea121 <132832017+Enea121@users.noreply.github.com>
2023-05-07 17:22:24 +0200

    Add files via upload

---
+++ warning.js
@@ -0,0 +1,8 @@
+<script>
+  var answer = confirm("Do you want to continue to the page?");
+  if (answer == true) {
+    window.location = "http://www.example.com";
+  } else {
+    // do nothing or redirect to another page
+  }
+</script>
